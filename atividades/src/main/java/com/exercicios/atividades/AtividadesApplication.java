1fa218f9cf24bad55144451bef291ccbddccbf6c
Yann <2411551235@fema.edu.br>
2025-06-07 17:26:33 -0300

    chore: Salva alterações pendentes da aplicação principal

--- atividades/src/main/java/com/exercicios/atividades/AtividadesApplication.java
+++ atividades/src/main/java/com/exercicios/atividades/AtividadesApplication.java
@@ -1,13 +1,41 @@
 package com.exercicios.atividades;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.exercicios.atividades.application.exercicio1.CanculadoraController.CalculadoraController;
+import com.exercicios.atividades.application.exercicio1.UseCase.DividirUseCase;
+import com.exercicios.atividades.application.exercicio1.UseCase.MultiplicarUseCase;
+import com.exercicios.atividades.application.exercicio1.UseCase.SomaUseCase;
+import com.exercicios.atividades.application.exercicio1.UseCase.SubtrairUseCase;
+import com.exercicios.atividades.infra.menus.MenuMetodo;
+import com.exercicios.atividades.infra.menus.MenuPrincipal;
+import com.exercicios.atividades.infra.interfaces.ControllerInterface;
+import com.exercicios.atividades.infra.interfaces.MenuInterface;
 
-@SpringBootApplication
+import java.util.Scanner;
+
 public class AtividadesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AtividadesApplication.class, args);
-	}
+		Scanner scanner = new Scanner(System.in);
 
+		SomaUseCase somaUseCase = new SomaUseCase();
+		SubtrairUseCase subtrairUseCase = new SubtrairUseCase();
+		MultiplicarUseCase multiplicarUseCase = new MultiplicarUseCase();
+		DividirUseCase dividirUseCase = new DividirUseCase();
+
+		ControllerInterface calculadoraController = new CalculadoraController(
+				dividirUseCase,
+				multiplicarUseCase,
+				somaUseCase,
+				subtrairUseCase,
+				scanner
+		);
+
+		MenuInterface menuMetodos = new MenuMetodo(scanner, calculadoraController);
+		MenuInterface menuPrincipal = new MenuPrincipal(scanner, menuMetodos);
+
+		menuPrincipal.exibir();
+
+		System.out.println("Aplicação encerrada.");
+		scanner.close();
+	}
 }
