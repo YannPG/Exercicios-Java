439597e1452e9a5eb82d64592b9f26ec31b45f54
Yann <2411551235@fema.edu.br>
2025-06-08 11:58:47 -0300

    feat: adicionar controllerFatorial ao menu

--- atividades/src/main/java/com/exercicios/atividades/infra/menus/MenuMetodo.java
+++ atividades/src/main/java/com/exercicios/atividades/infra/menus/MenuMetodo.java
@@ -9,12 +9,14 @@ public class MenuMetodo implements MenuInterface {
 
     private final ControllerInterface calculadoraController;
     private final ControllerInterface verificarPalindromoController;
+    private final ControllerInterface calcularFatorialController;
     private final Scanner scanner;
 
-    public MenuMetodo(Scanner scanner, ControllerInterface calculadoraController, ControllerInterface verificarPalindromoController) {
+    public MenuMetodo(Scanner scanner, ControllerInterface calculadoraController, ControllerInterface verificarPalindromoController, ControllerInterface calcularFatorialController) {
         this.scanner = scanner;
         this.calculadoraController = calculadoraController;
         this.verificarPalindromoController = verificarPalindromoController;
+        this.calcularFatorialController = calcularFatorialController;
     }
 
     @Override
@@ -24,6 +26,7 @@ public class MenuMetodo implements MenuInterface {
             System.out.println("\n===== MENU DE MÉTODOS =====");
             System.out.println("1. Calculadora Simples");
             System.out.println("2. Verifica Palíndromos");
+            System.out.println("3. Calcular Fatorial");
             System.out.println("0. Voltar");
             System.out.print("Escolha uma opção: ");
 
@@ -35,6 +38,9 @@ public class MenuMetodo implements MenuInterface {
             }
             if(opcao == 2){
                 this.verificarPalindromoController.executar();
+            }
+            if(opcao == 3){
+                this.calcularFatorialController.executar();
             }
 
         } while (opcao != 0);
