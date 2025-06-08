0689043c547a8c696c3b54b415cfa0a3e706eedb
Yann <2411551235@fema.edu.br>
2025-06-08 13:58:26 -0300

    feat: adicionar os useCase de temperatura

---
+++ atividades/src/main/java/com/exercicios/atividades/application/exercicio4/ConversorTemperaturaUseCase/CelsiusFahrenheitUseCase.java
@@ -0,0 +1,7 @@
+package com.exercicios.atividades.application.exercicio4.ConversorTemperaturaUseCase;
+
+public class CelsiusFahrenheitUseCase {
+    public Double celsiusFahrenheit (Double celsius) {
+        return (celsius * 9/5) + 32;
+    }
+}
---
+++ atividades/src/main/java/com/exercicios/atividades/application/exercicio4/ConversorTemperaturaUseCase/FahrenheitCelsiusUseCase.java
@@ -0,0 +1,7 @@
+package com.exercicios.atividades.application.exercicio4.ConversorTemperaturaUseCase;
+
+public class FahrenheitCelsiusUseCase {
+    public Double fahrenheitCelsius(Double fahrenheit) {
+        return (fahrenheit - 32) * 5/9;
+    }
+}
