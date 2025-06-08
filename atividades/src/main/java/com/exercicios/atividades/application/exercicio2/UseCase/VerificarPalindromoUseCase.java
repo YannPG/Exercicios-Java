acc9b36582db6bc69ab3c40ad8a6b9734b6e36e2
Yann <2411551235@fema.edu.br>
2025-06-08 11:54:18 -0300

    refactor: Alterar metodo de verificar palindromo para verificar se há espaços

--- atividades/src/main/java/com/exercicios/atividades/application/exercicio2/UseCase/VerificarPalindromoUseCase.java
+++ atividades/src/main/java/com/exercicios/atividades/application/exercicio2/UseCase/VerificarPalindromoUseCase.java
@@ -3,10 +3,7 @@ package com.exercicios.atividades.application.exercicio2.UseCase;
 public class VerificarPalindromoUseCase {
     public Boolean verificarPalindomo(String mensagem){
         String mensagemLimpa = mensagem.replaceAll("\\s+", "").toLowerCase();
-        String palindromo = "";
-        for(int i = mensagemLimpa.length() - 1; i >= 0; i--) {
-            palindromo += mensagemLimpa.charAt(i);
-        }
+        String palindromo = new StringBuilder(mensagemLimpa).reverse().toString();
         return mensagemLimpa.equals(palindromo);
     }
 }
