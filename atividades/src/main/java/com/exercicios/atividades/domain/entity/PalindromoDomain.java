96d84e805d2f0b54fed6698e61de87a6aae32c03
yann_ <yann_pg@hotmail.com>
2025-06-07 23:31:03 -0300

    feat: implementar PalindromoDomain

---
+++ atividades/src/main/java/com/exercicios/atividades/domain/entity/PalindromoDomain.java
@@ -0,0 +1,14 @@
+package com.exercicios.atividades.domain.entity;
+
+public class PalindromoDomain {
+
+    private String mensagem;
+
+    public PalindromoDomain(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+}
