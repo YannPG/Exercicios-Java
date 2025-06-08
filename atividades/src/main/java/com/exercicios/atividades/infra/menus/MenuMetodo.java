b20431b6a097c7caa1cdc06bbe678f33bcf8b151
Yann <2411551235@fema.edu.br>
2025-06-08 14:02:37 -0300

    feat: adicionar a implementação de temperaturaController ao menuMetodo

--- atividades/src/main/java/com/exercicios/atividades/infra/menus/MenuMetodo.java
+++ atividades/src/main/java/com/exercicios/atividades/infra/menus/MenuMetodo.java
@@ -10,13 +10,15 @@ public class MenuMetodo implements MenuInterface {
     private final ControllerInterface calculadoraController;
     private final ControllerInterface verificarPalindromoController;
     private final ControllerInterface calcularFatorialController;
+    private final ControllerInterface conversorTemperaturaController;
     private final Scanner scanner;
 
-    public MenuMetodo(Scanner scanner, ControllerInterface calculadoraController, ControllerInterface verificarPalindromoController, ControllerInterface calcularFatorialController) {
+    public MenuMetodo(Scanner scanner, ControllerInterface calculadoraController, ControllerInterface verificarPalindromoController, ControllerInterface calcularFatorialController,ControllerInterface conversorTemperaturaController) {
         this.scanner = scanner;
         this.calculadoraController = calculadoraController;
         this.verificarPalindromoController = verificarPalindromoController;
         this.calcularFatorialController = calcularFatorialController;
+        this.conversorTemperaturaController = conversorTemperaturaController;
     }
 
     @Override
@@ -27,6 +29,7 @@ public class MenuMetodo implements MenuInterface {
             System.out.println("1. Calculadora Simples");
             System.out.println("2. Verifica Palíndromos");
             System.out.println("3. Calcular Fatorial");
+            System.out.println("4. Conversor de temperaturas");
             System.out.println("0. Voltar");
             System.out.print("Escolha uma opção: ");
 
@@ -41,6 +44,9 @@ public class MenuMetodo implements MenuInterface {
             }
             if(opcao == 3){
                 this.calcularFatorialController.executar();
+            }
+            if(opcao == 4){
+                this.conversorTemperaturaController.executar();
             }
 
         } while (opcao != 0);
