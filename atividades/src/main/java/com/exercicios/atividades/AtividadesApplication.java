e85e606c1f5d3bb242298709fd06953c210f1b81
yann_ <yann_pg@hotmail.com>
2025-06-07 23:34:17 -0300

    feat: adicionar a instanciação de palindro na main

--- atividades/src/main/java/com/exercicios/atividades/AtividadesApplication.java
+++ atividades/src/main/java/com/exercicios/atividades/AtividadesApplication.java
@@ -5,6 +5,8 @@ import com.exercicios.atividades.application.exercicio1.UseCase.DividirUseCase;
 import com.exercicios.atividades.application.exercicio1.UseCase.MultiplicarUseCase;
 import com.exercicios.atividades.application.exercicio1.UseCase.SomaUseCase;
 import com.exercicios.atividades.application.exercicio1.UseCase.SubtrairUseCase;
+import com.exercicios.atividades.application.exercicio2.UseCase.VerificarPalindromoUseCase;
+import com.exercicios.atividades.application.exercicio2.VerificarPalindromos.VerificarPalindromoController;
 import com.exercicios.atividades.infra.menus.MenuMetodo;
 import com.exercicios.atividades.infra.menus.MenuPrincipal;
 import com.exercicios.atividades.infra.interfaces.ControllerInterface;
@@ -17,10 +19,13 @@ public class AtividadesApplication {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
+		//Calculadora
 		SomaUseCase somaUseCase = new SomaUseCase();
 		SubtrairUseCase subtrairUseCase = new SubtrairUseCase();
 		MultiplicarUseCase multiplicarUseCase = new MultiplicarUseCase();
 		DividirUseCase dividirUseCase = new DividirUseCase();
+		//VerificarPalindromo
+		VerificarPalindromoUseCase verificarPalindromoUseCase = new VerificarPalindromoUseCase();
 
 		ControllerInterface calculadoraController = new CalculadoraController(
 				dividirUseCase,
@@ -30,7 +35,12 @@ public class AtividadesApplication {
 				scanner
 		);
 
-		MenuInterface menuMetodos = new MenuMetodo(scanner, calculadoraController);
+		ControllerInterface verificarPalindromoController = new VerificarPalindromoController(
+				verificarPalindromoUseCase,
+				scanner
+		);
+
+		MenuInterface menuMetodos = new MenuMetodo(scanner, calculadoraController, verificarPalindromoController);
 		MenuInterface menuPrincipal = new MenuPrincipal(scanner, menuMetodos);
 
 		menuPrincipal.exibir();
