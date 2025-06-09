ef4350757e53a0da3f65b859b780594989e5896f
Yann <yann_pg@hotmail.com>
2025-06-09 09:10:39 -0300

    feat: Adicionar TrocaValores ao MenuManipulacaoObjetos

--- atividades/src/main/java/com/exercicios/atividades/infra/menus/MenuManipulacaoObjetos.java
+++ atividades/src/main/java/com/exercicios/atividades/infra/menus/MenuManipulacaoObjetos.java
@@ -9,11 +9,13 @@ public class MenuManipulacaoObjetos implements MenuInterface {
 
     private final Scanner scanner;
     private final ControllerInterface exercicioDadosObjetoController;
+    private final ControllerInterface trocaValoresController;
 
 
-    public MenuManipulacaoObjetos(Scanner scanner, ControllerInterface exercicioDadosObjetoController) {
+    public MenuManipulacaoObjetos(Scanner scanner, ControllerInterface exercicioDadosObjetoController, ControllerInterface trocaValoresController) {
         this.scanner = scanner;
         this.exercicioDadosObjetoController = exercicioDadosObjetoController;
+        this.trocaValoresController = trocaValoresController;
     }
 
     @Override
@@ -22,6 +24,7 @@ public class MenuManipulacaoObjetos implements MenuInterface {
         do {
             System.out.println("\n===== MENU DE OBJETOS =====");
             System.out.println("1. Manipulando Dados de um Objeto\n");
+            System.out.println("2. Trocando Valores\n");
             System.out.println("0. Voltar");
             System.out.print("Escolha uma opção: ");
 
@@ -30,6 +33,9 @@ public class MenuManipulacaoObjetos implements MenuInterface {
 
             if (opcao == 1) {
                 this.exercicioDadosObjetoController.executar();
+            }
+            if (opcao == 2){
+                this.trocaValoresController.executar();
             }
 
         } while (opcao != 0);
