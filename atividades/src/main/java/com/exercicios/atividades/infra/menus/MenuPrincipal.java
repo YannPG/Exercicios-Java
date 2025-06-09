15b38e3be86967135ee042763fd9b51700c5a0e1
Yann <yann_pg@hotmail.com>
2025-06-09 10:15:56 -0300

    feat: adicionar o menu de classe e objetos ao menu principal

--- atividades/src/main/java/com/exercicios/atividades/infra/menus/MenuPrincipal.java
+++ atividades/src/main/java/com/exercicios/atividades/infra/menus/MenuPrincipal.java
@@ -9,13 +9,15 @@ public class MenuPrincipal implements MenuInterface {
     private final MenuInterface proximoMenu;
     private final MenuInterface menuMatriz;
     private final MenuInterface menuManipulacaoObjetos;
+    private final MenuInterface menuClassesObjetos;
     private final Scanner scanner;
 
-    public MenuPrincipal(Scanner scanner, MenuInterface proximoMenu, MenuInterface menuMatriz, MenuInterface menuManipulacaoObjetos) {
+    public MenuPrincipal(Scanner scanner, MenuInterface proximoMenu, MenuInterface menuMatriz, MenuInterface menuManipulacaoObjetos, MenuInterface menuClassesObjetos) {
         this.scanner = scanner;
         this.proximoMenu = proximoMenu;
         this.menuMatriz = menuMatriz;
         this.menuManipulacaoObjetos = menuManipulacaoObjetos;
+        this.menuClassesObjetos = menuClassesObjetos;
     }
 
     @Override
@@ -26,6 +28,7 @@ public class MenuPrincipal implements MenuInterface {
             System.out.println("1. Métodos");
             System.out.println("2. Matriz");
             System.out.println("3. Manipulação de objetos");
+            System.out.println("4. Classes e Objetos");
             System.out.println("0. Sair");
             System.out.print("Escolha uma opção: ");
 
@@ -40,6 +43,9 @@ public class MenuPrincipal implements MenuInterface {
             }
             if(opcao == 3){
                 this.menuManipulacaoObjetos.exibir();
+            }
+            if(opcao == 4){
+                this.menuClassesObjetos.exibir();
             }
 
         } while (opcao != 0);
